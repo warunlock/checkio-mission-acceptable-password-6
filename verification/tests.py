ad2d1f5ab747bf2ec85b51d5cbb65e5238e82499
Oleksandr Zozulia <freemanlex@gmail.com>
2022-08-28 18:14:39 +0300

    next-api uk hints uk-hints

--- verification/tests.py
+++ verification/tests.py
@@ -1,88 +1,87 @@
 """
-TESTS is a dict with all of your tests.
-Keys for this will be the categories' names.
-Each test is a dict with
-    "input" -- input data for a user function
+TESTS is a dict with all you tests.
+Keys for this will be categories' names.
+Each test is dict with
+    "input" -- input data for user function
     "answer" -- your right answer
-    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+    "explanation" -- not necessary key, it's using for additional info in animation.
 """
-
 
 TESTS = {
     "Basics": [
         {
-            "input": ['short'],
-            "answer": False,
+            "input": ["short"],
+            "answer": False
         },
         {
-            "input": ['short54'],
-            "answer": True,
+            "input": ["short54"],
+            "answer": True
         },
         {
-            "input": ['muchlonger'],
-            "answer": True,
+            "input": ["muchlonger"],
+            "answer": True
         },
         {
-            "input": ['ashort'],
-            "answer": False,
+            "input": ["ashort"],
+            "answer": False
         },
         {
-            "input": ['muchlonger5'],
-            "answer": True,
+            "input": ["muchlonger5"],
+            "answer": True
         },
         {
-            "input": ['sh5'],
-            "answer": False,
+            "input": ["sh5"],
+            "answer": False
         },
         {
-            "input": ['1234567'],
-            "answer": False,
+            "input": ["1234567"],
+            "answer": False
         },
         {
-            "input": ['12345678910'],
-            "answer": True,
+            "input": ["12345678910"],
+            "answer": True
         },
         {
-            "input": ['password12345'],
-            "answer": False,
+            "input": ["password12345"],
+            "answer": False
         },
         {
-            "input": ['PASSWORD12345'],
-            "answer": False,
+            "input": ["PASSWORD12345"],
+            "answer": False
         },
         {
-            "input": ['pass1234word'],
-            "answer": True,
+            "input": ["pass1234word"],
+            "answer": True
         },
         {
-            "input": ['aaaaaa1'],
-            "answer": False,
+            "input": ["aaaaaa1"],
+            "answer": False
         },
         {
-            "input": ['aaaaaabbbbb'],
-            "answer": False,
+            "input": ["aaaaaabbbbb"],
+            "answer": False
         },
         {
-            "input": ['aaaaaabb1'],
-            "answer": True,
+            "input": ["aaaaaabb1"],
+            "answer": True
         },
         {
-            "input": ['abc1'],
-            "answer": False,
+            "input": ["abc1"],
+            "answer": False
         },
         {
-            "input": ['abbcc12'],
-            "answer": True,
+            "input": ["abbcc12"],
+            "answer": True
         },
         {
-            "input": ['aaaaaaabbaaaaaaaab'],
-            "answer": False,
-        },
+            "input": ["aaaaaaabbaaaaaaaab"],
+            "answer": False
+        }
     ],
     "Extra": [
         {
-            "input": ['this is password'],
-            "answer": False,
+            "input": ["this is password"],
+            "answer": False
         }
     ]
 }
